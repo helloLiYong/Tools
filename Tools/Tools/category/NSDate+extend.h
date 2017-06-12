52534f71856c39b76c90d1b6ae31e4c65b95433c
LY <LY@163.com>
2017-06-12 12:34:57 +0800

    初始化Tools项目

---
+++ Tools/Tools/category/NSDate+extend.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+extend.h
+//  Tools
+//
+//  Created by 李勇 on 2017/6/12.
+//  Copyright © 2017年 李勇. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (extend)
++(NSDictionary *)getDateInfoFromDate:(NSDate *)date;
+@end
